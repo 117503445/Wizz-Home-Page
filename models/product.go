1f9485e6d1118999d243d16c5b6d8896be7dfad9
117503445 <t117503445@gmail.com>
2020-02-02 10:14:53 +0800

    Update product.go

--- models/product.go
+++ models/product.go
@@ -15,13 +15,9 @@ type Product struct {
 	//项目类型,0 - 校企合作,1 - 校园合作,2 - 校内自研
 	ProjectType int `example:"" enums:"0,1"`
 
-	//项目图标的 Base64 字符串
-	ImgAvatar string `example:""`
 	//项目图标的 Url
 	UrlAvatar string `example:""`
 
-	//合作方Logo的 Base64 字符串
-	ImgPartnerLogo string `example:""`
 	//合作方Logo的 Url
 	UrlPartnerLogo string `example:""`
 
@@ -30,13 +26,9 @@ type Product struct {
 	//项目背景图片的 Url
 	UrlBackground string `example:""`
 
-	//项目截图的 Base64 字符串
-	ImgScreenshot []string `gorm:"type:string[]" example:""`
 	//项目截图的 Url
 	UrlScreenshot []string `gorm:"type:string[]" example:""`
 
-	//二维码的 Base64 字符串
-	ImgProCode string `example:""`
 	//二维码的 Url
 	UrlProCode string `example:""`
 }
