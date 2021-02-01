243d4457285664f4043bce3ab5affce157fe798b
117503445 <t117503445@gmail.com>
2021-02-01 17:49:48 +0800

    feat: codegen template_fill

---
+++ script/template_fill/main.py
@@ -0,0 +1,30 @@
+from htutil import file
+import json
+from pathlib import Path
+import toml
+
+
+def make_template(template: str, value: dict) -> str:
+    for d in value:
+        template = template.replace('${'+d+'}', value[d])
+    return template
+
+
+def main():
+
+    config = file.read_all_text('config.toml')
+    config = toml.loads(config)
+
+    path_in = Path('in') / config['in']['path']
+    template = file.read_all_text(path_in / 'template.txt')
+    path_out = Path('out')
+    for p in path_in.glob('*.json'):
+        in_value = json.loads(file.read_all_text(p))
+        result = make_template(template, in_value)
+        print(result)
+        file.write_all_text(
+            path_out / ('.'.join(p.name.split('.')[:-1]) + '.txt'), result)
+
+
+if __name__ == '__main__':
+    main()
