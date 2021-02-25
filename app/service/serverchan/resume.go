e92b80728c9e0bd152670373c438f03c149a0d0b
xiaoxi <1577912515@qq.com>
2021-02-25 13:54:13 +0800

    feat: message create

--- app/service/serverchan/resume.go
+++ app/service/serverchan/resume.go
@@ -34,6 +34,22 @@ func ResumeRemind() {
 		title := fmt.Sprintf("未提交%v的面评", resume.Name)
 		content := fmt.Sprintf("距离投递者投递简历已经过去一周啦~请尽快到此url填写投递者的面试结果以及面试评价，否则将会每天提醒哦\n\n（ps:不需要面试也需要选择是否通过，填写理由哦）\n\n[url](%v)\n\n---\n\n【快速回顾%v的简历】\n\n%v %v %v\n（联系电话：%v）（微信：%v）（qq：%v）\n\n%v\n\n%v", url, resume.Name, resume.Name, resume.Grade, resume.CollegeMajor, resume.TelephoneNumber, resume.WechatNumber, resume.QqNumber, experienceStr, resume.Describe)
 		Push(interviewer.ServerchanId, title, content)
+		var message model.Messages
+		err = dao.Messages.Where("resume_id", resume.Id).Struct(&message)
+		if err != nil {
+			message.Content = fmt.Sprintf("面试官%v未及时处理简历", interviewer.Name)
+			message.ResumeId = resume.Id
+			message.CreateTime = gtime.TimestampMilli()
+			message.ReadStatus = 0
+			if _, err = dao.Messages.Insert(message); err != nil {
+				g.Log().Line().Error(err)
+			}
+		} else {
+			message.CreateTime = gtime.TimestampMilli()
+			if _, err = dao.Messages.Data(message).Where("id", message.Id).Update(); err != nil {
+				g.Log().Line().Error(err)
+			}
+		}
 		time.Sleep(3 * time.Second)
 	}
 
