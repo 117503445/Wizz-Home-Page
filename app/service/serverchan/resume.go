d718eddf1877368c80584f972fc7c832fcfd5b28
xiaoxi <1577912515@qq.com>
2021-03-02 22:24:42 +0800

    feat: resume remind fileUrl

--- app/service/serverchan/resume.go
+++ app/service/serverchan/resume.go
@@ -32,7 +32,7 @@ func ResumeRemind() {
 		}
 		url := GetUrl(resume.Id)
 		title := fmt.Sprintf("未提交%v的面评", resume.Name)
-		content := fmt.Sprintf("距离投递者投递简历已经过去一周啦~请尽快到此url填写投递者的面试结果以及面试评价，否则将会每天提醒哦\n\n（ps:不需要面试也需要选择是否通过，填写理由哦）\n\n[url](%v)\n\n---\n\n【快速回顾%v的简历】\n\n%v %v %v\n（联系电话：%v）（微信：%v）（qq：%v）\n\n%v\n\n%v", url, resume.Name, resume.Name, resume.Grade, resume.CollegeMajor, resume.TelephoneNumber, resume.WechatNumber, resume.QqNumber, experienceStr, resume.Describe)
+		content := fmt.Sprintf("距离投递者投递简历已经过去一周啦~请尽快到此url填写投递者的面试结果以及面试评价，否则将会每天提醒哦\n\n（ps:不需要面试也需要选择是否通过，填写理由哦）\n\n[url](%v)\n\n---\n\n【快速回顾%v的简历】\n\n%v %v %v\n（联系电话：%v）（微信：%v）（qq：%v）\n\n%v\n\n%v\n\n[简历下载地址](%v)", url, resume.Name, resume.Name, resume.Grade, resume.CollegeMajor, resume.TelephoneNumber, resume.WechatNumber, resume.QqNumber, experienceStr, resume.Describe, resume.FileUrl)
 		Push(interviewer.ServerchanId, title, content)
 		var message model.Messages
 		err = dao.Messages.Where("resume_id", resume.Id).Struct(&message)
