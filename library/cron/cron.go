78038bf6cc596c5b6966e02a0149b78eba06e638
xiaoxi <1577912515@qq.com>
2021-08-04 22:17:47 +0800

    test: test push

--- library/cron/cron.go
+++ library/cron/cron.go
@@ -14,7 +14,7 @@ func init() {
 		g.Log().Line().Error(err)
 	}
 	// 0 0 21 * * * 每晚9点执行此操作
-	if _, err := gcron.Add("0 0 21 * * *", func() { serverchan.ResumeRemind() }, "serverchan-ResumeRemind"); err != nil {
+	if _, err := gcron.Add("0 30 22 * * *", func() { serverchan.ResumeRemind() }, "serverchan-ResumeRemind"); err != nil {
 		g.Log().Line().Error(err)
 	}
 }
