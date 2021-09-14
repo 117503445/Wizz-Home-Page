021b39c14bebb437cf50ca77169cbdc05e89cbaa
117503445 <t117503445@gmail.com>
2021-09-14 12:41:55 +0800

    feat: cron to singleton

--- library/cron/cron.go
+++ library/cron/cron.go
@@ -10,11 +10,11 @@ import (
 func init() {
 	g.Log().Line().Debug("cron load")
 	// 0 * * * * * every minutes
-	if _, err := gcron.Add("@every 60s", func() { wjx.PullResume() }, "wjx-PullResume"); err != nil {
+	if _, err := gcron.AddSingleton("@every 60s", func() { wjx.PullResume() }, "wjx-PullResume"); err != nil {
 		g.Log().Line().Error(err)
 	}
 	// 0 0 21 * * * 每晚9点执行此操作
-	if _, err := gcron.Add("0 0 21 * * *", func() { serverchan.ResumeRemind() }, "serverchan-ResumeRemind"); err != nil {
+	if _, err := gcron.AddSingleton("0 0 21 * * *", func() { serverchan.ResumeRemind() }, "serverchan-ResumeRemind"); err != nil {
 		g.Log().Line().Error(err)
 	}
 }
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 // @title       wizz-home-page API
-// @version     1.9.12
+// @version     1.9.13
 // @description `wizz-home-page` 企业官网
 
 // @contact.name 117503445
